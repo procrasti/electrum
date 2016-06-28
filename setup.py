43453053bc6128128cb4cc6142d727f8337e943e
procrasti <procrasti@k5-stats.org>
2016-06-29 02:24:48 +0800

    datafiles creates a snadboxviolation.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info[:3] < (2, 7, 0):
 
 data_files = []
 
-if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
+if False and platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
     parser = argparse.ArgumentParser()
     parser.add_argument('--root=', dest='root_path', metavar='dir', default='/')
     opts, _ = parser.parse_known_args(sys.argv[1:])
